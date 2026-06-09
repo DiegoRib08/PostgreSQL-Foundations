582815b673a01eae97cd1a01221093985fafdb51
Diego <ribeirodiego2008@gmail.com>
2026-06-09 11:57:51 -0400

    Used DISTINCT to return course info

---
+++ Module 3/task4.sql
@@ -0,0 +1,10 @@
+-- Use 'DISTINCT' to list unique values in a column
+SELECT DISTINCT semester FROM courses;
+/* Shows the semesters that have courses currently planned
+Results
+┌───────────┐
+│ semester  │
+├───────────┤
+│ Fall 2026 │
+└───────────┘
+*/
